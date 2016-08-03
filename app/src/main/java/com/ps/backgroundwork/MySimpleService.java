44aec1f15bde5fd2c94858d0ff09a46052cb2ea3
Karan <karan.kumar.c@gmail.com>
2016-08-03 13:44:12 +0530

    Service with looper.

--- app/src/main/java/com/ps/backgroundwork/MySimpleService.java
+++ app/src/main/java/com/ps/backgroundwork/MySimpleService.java
@@ -2,25 +2,61 @@ package com.ps.backgroundwork;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Handler;
+import android.os.HandlerThread;
 import android.os.IBinder;
+import android.os.Message;
 
 import java.io.FileOutputStream;
 
 public class MySimpleService extends Service {
 
-    public MySimpleService() {
-    }
+    HandlerThread mHandlerThread;
+    Handler mHandler;
 
     @Override
     public IBinder onBind(Intent intent) {
-        // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
+        return null;
+    }
+
+
+    // Put the handler thread and handler in place
+    @Override
+    public void onCreate() {
+        mHandlerThread = new HandlerThread("MySimpleService");
+        mHandlerThread.start();
+        mHandler = new Handler(mHandlerThread.getLooper()){
+            @Override
+            public void handleMessage(Message msg) {
+                Intent intent = (Intent) msg.obj;
+                doWork(intent);
+            }
+        };
+    }
+
+    @Override
+    public void onDestroy() {
+        mHandlerThread.quit();
+        mHandlerThread = null;
+        mHandler = null;
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
 
         LogHelper.logThreadId("onStartCommand");
+
+        Message msg = mHandler.obtainMessage();
+        msg.obj = intent;
+        msg.sendToTarget();
+
+
+        return 0;
+    }
+
+    private void doWork(Intent intent){
+
+        LogHelper.logThreadId("doWork");
 
         String messageText = intent.getStringExtra("MessageText");
 
@@ -29,6 +65,5 @@ public class MySimpleService extends Service {
             FileHelper.slowWrite(outStream, messageText);
         }
         FileHelper.closeOutStream(outStream);
-        return 0;
     }
 }
